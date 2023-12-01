ac1ea5905f999772a77fff9b07894c55967c731b
Deagel1337 <62595994+Deagel1337@users.noreply.github.com>
2023-12-01 18:05:40 +0100

    solution 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
-
+#include <fstream>
+using namespace std;
 int main(){
-    std::cout << "Advent of Code" << std::endl;
+    ifstream input("input.txt");
+    if(input.is_open()){
+    string line;
+    int sum = 0;
+    while(getline(input,line)){
+        string characterInt = "";
+        for(unsigned long i = 0; i < line.length();i++){
+            char currenctChar = line.c_str()[i];
+            if(isdigit(currenctChar)){
+                characterInt += currenctChar;
+            }
+        }
+        if(characterInt.length() > 1)
+        {
+            string neededNumber = "";
+            neededNumber += characterInt[0];
+            neededNumber += characterInt[characterInt.length()-1];
+            int number = stoi(neededNumber);
+            sum += number;
+        }
+        else
+        {
+            string doubledNumber = characterInt + characterInt;
+            sum += stoi(doubledNumber);
+        }
+    }
+    printf("Sum: %i",sum);
+    input.close();
+    }
     return 0;
 }
